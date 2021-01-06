6462c97492b99a77d1db651fa9e65c926c5be4f2
testcenter-mm <melagho@gmail.com>
2021-01-06 15:26:31 +0100

    zweite Änderung

--- SeleniumCSharpNetcore/UnitTest1.cs
+++ SeleniumCSharpNetcore/UnitTest1.cs
@@ -21,6 +21,9 @@ namespace SeleniumCSharpNetcore
             Driver.Navigate().GoToUrl("https://demowf.aspnetawesome.com/");
             Driver.FindElement(By.Id("ContentPlaceHolder1_Meal")).SendKeys("Tomato");
             Driver.FindElement(By.XPath("//input[@name='ctl00$ContentPlaceHolder1$ChildMeal1']/following-sibling::div[text()='Celery']")).Click();
+
+            // Element in einem Combobox suchen. Andere Art um Elemente in Combobox zu suchen
+            Driver.FindElement(By.XPath("//div[@id='ContentPlaceHolder1_AllMealsCombo-dropmenu']//li[text()='Almond']")).Click();
             Console.WriteLine("Test1");
 
             Assert.Pass();
